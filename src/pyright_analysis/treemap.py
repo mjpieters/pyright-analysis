f6124dfdb59a8f0942bf32ce1ff674dab0f66b96
Martijn Pieters <mj@zopatista.com>
2025-01-22 13:45:40 +0000

    feat: Add a footer linking to the project
    To position the footer correctly, the graph Title element has been
    repurposed for this and an annotation has been added to take over the
    placement of the title.

--- src/pyright_analysis/treemap.py
+++ src/pyright_analysis/treemap.py
@@ -2,6 +2,7 @@ from typing import Any, NamedTuple, Self
 
 import plotly.graph_objects as go  # pyright: ignore[reportMissingTypeStubs]
 
+from ._utils import project_link
 from .schema import Symbol, SymbolName, TypeCompletenessReport
 
 
@@ -54,10 +55,15 @@ def _collate(report: TypeCompletenessReport) -> dict[SymbolName, ModuleInfo]:
     return per_module
 
 
-def to_treemap(report: TypeCompletenessReport) -> go.Figure:
+def to_treemap(
+    report: TypeCompletenessReport, generator_link: str | None = None
+) -> go.Figure:
     per_module = _collate(report)
     ordered = sorted(per_module)
     info = [per_module[name] for name in ordered]
+
+    title = f"Pyright type completeness report for {report.package_name}"
+    attribution = f"Generated with {generator_link or project_link()}"
 
     return go.Figure(
         data=go.Treemap(
@@ -91,12 +97,37 @@ def to_treemap(report: TypeCompletenessReport) -> go.Figure:
         ),
         layout=go.Layout(
             margin=go.layout.Margin(t=75, l=25, r=25, b=25),
-            title=go.layout.Title(
-                text=f"Pyright type completeness report for {report.package_name}"
-            ),
             coloraxis=go.layout.Coloraxis(
                 colorbar=go.layout.coloraxis.ColorBar(tickformat=".0%"),
                 colorscale="rdylgn",
+            ),
+            # layout trick from plotly/plotly.py#4873, putting the title in
+            # an annotation and the footnote in a title gives you much better
+            # control over the attribution placement.
+            annotations=[
+                go.layout.Annotation(
+                    showarrow=False,
+                    text=title,
+                    yref="paper",
+                    yanchor="bottom",
+                    y=1.04,
+                    xref="paper",
+                    xanchor="center",
+                    x=0.5,
+                    font=go.layout.annotation.Font(size=20),
+                )
+            ],
+            legend=go.layout.Legend(
+                yanchor="top", y=1, xanchor="right", x=1, borderwidth=1
+            ),
+            title=go.layout.Title(
+                text=attribution,
+                yref="container",
+                y=0.005,
+                xref="paper",
+                xanchor="right",
+                x=1,
+                font=go.layout.title.Font(size=12, style="italic"),
             ),
         ),
     )
